384b21f1ca641a22748e5c033e7c514c439a5975
Richard <olella93@gmail.com>
2025-06-14 02:50:59 +0300

    Added GET endpoint to fetch all restaurants

--- server/controllers/restaurant_controller.py
+++ server/controllers/restaurant_controller.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from server.models.restaurant import Restaurant
+from server.models.restaurant_pizza import RestaurantPizza
+from server.app import db
 
-restaurant_bp = Blueprint('restaurant_bp', __name__)
+restaurant_bp = Blueprint('restaurants', __name__, url_prefix="/restaurants")
+
+@restaurant_bp.route("", methods=["GET"])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    data = [
+        {"id": r.id, "name": r.name, "address": r.address}
+        for r in restaurants
+    ]
+    return jsonify(data), 200
+
+
