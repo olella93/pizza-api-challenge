2270b9e77cdb61718b0d8a1396b6d4158615e4c8
Richard <olella93@gmail.com>
2025-06-14 02:02:44 +0300

    restaurant model created

---
+++ server/models/restaurant.py
@@ -0,0 +1,15 @@
+from server.app import db
+from sqlalchemy.orm import relationship
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    restaurant_pizzas = relationship(
+        "RestaurantPizza",
+        back_populates="restaurant",
+        cascade="all, delete"
+    )
