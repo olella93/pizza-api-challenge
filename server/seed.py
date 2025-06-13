e0cc52ec9770b7924eee079f78d554dbdfa56ade
Richard <olella93@gmail.com>
2025-06-14 02:32:20 +0300

    RestaurantPizzas created on seed.py

--- server/seed.py
+++ server/seed.py
@@ -25,4 +25,13 @@ with app.app_context():
     db.session.add_all([r1, r2, p1, p2, p3])
     db.session.commit()
 
-   
+    # Create RestaurantPizzas (joins)
+    rp1 = RestaurantPizza(price=10, pizza_id=p1.id, restaurant_id=r1.id)
+    rp2 = RestaurantPizza(price=15, pizza_id=p2.id, restaurant_id=r1.id)
+    rp3 = RestaurantPizza(price=12, pizza_id=p1.id, restaurant_id=r2.id)
+    rp4 = RestaurantPizza(price=9, pizza_id=p3.id, restaurant_id=r2.id)
+
+    db.session.add_all([rp1, rp2, rp3, rp4])
+    db.session.commit()
+
+    print("✅ Done seeding!")
