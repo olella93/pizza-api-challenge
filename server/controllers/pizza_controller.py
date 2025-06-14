325994761d8c6bd6eb29b2597ce47b4a840b5cd7
Richard <olella93@gmail.com>
2025-06-14 03:07:18 +0300

    Added GET endpoint to fetch all pizzas

--- server/controllers/pizza_controller.py
+++ server/controllers/pizza_controller.py
@@ -1,4 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from server.models.pizza import Pizza
 
-pizza_bp = Blueprint('pizza_bp', __name__)
+pizza_bp = Blueprint('pizzas', __name__, url_prefix="/pizzas")
 
+@pizza_bp.route("", methods=["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    data = [
+        {"id": p.id, "name": p.name, "ingredients": p.ingredients}
+        for p in pizzas
+    ]
+    return jsonify(data), 200
